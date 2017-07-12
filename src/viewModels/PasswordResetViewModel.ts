41f3c47195c574b851cddde308e3f3166d0a4ed4
adnan-kamili <adnan.kamili@gmail.com>
2017-07-12 13:13:22 +0530

    removed js files

---
+++ src/viewModels/PasswordResetViewModel.ts
@@ -0,0 +1,13 @@
+import { MaxLength, IsEmail, Length, IsBase64, IsAscii } from "class-validator";
+
+export class PasswordResetViewModel {
+    @IsEmail()
+    email: string;
+
+    @IsAscii()
+    @Length(6, 72)
+    password: string;
+
+    @IsBase64()
+    token: string;
+}
